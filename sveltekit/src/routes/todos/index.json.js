f14fbcb48899707f8b817d618d8878a1b872b297
AaronM <info@aamservices.uk>
2021-07-06 14:24:00 +0100

    upgrade sk version

--- sveltekit/src/routes/todos/index.json.js
+++ sveltekit/src/routes/todos/index.json.js
@@ -2,8 +2,8 @@ import { api } from './_api';
 
 // GET /todos.json
 export const get = async (request) => {
-	// request.context.userid comes from src/hooks.js
-	const response = await api(request, `todos/${request.context.userid}`);
+	// request.locals.userid comes from src/hooks.js
+	const response = await api(request, `todos/${request.locals.userid}`);
 
 	if (response.status === 404) {
 		// user hasn't created a todo list.
@@ -16,7 +16,7 @@ export const get = async (request) => {
 
 // POST /todos.json
 export const post = async (request) => {
-	const response = await api(request, `todos/${request.context.userid}`, {
+	const response = await api(request, `todos/${request.locals.userid}`, {
 		// because index.svelte posts a FormData object,
 		// request.body is _also_ a (readonly) FormData
 		// object, which allows us to get form data
--- sveltekit/src/routes/todos/index.svelte
+++ sveltekit/src/routes/todos/index.svelte
@@ -1,20 +1,20 @@
 <script context="module">
 	import { enhance } from '$lib/form';
-	
+
 	// see https://kit.svelte.dev/docs#loading
 	export const load = async ({ fetch }) => {
 		const res = await fetch('/todos.json');
-	
+
 		if (res.ok) {
 			const todos = await res.json();
-	
+
 			return {
 				props: { todos }
 			};
 		}
-	
+
 		const { message } = await res.json();
-	
+
 		return {
 			error: new Error(message)
 		};
@@ -24,12 +24,12 @@
 <script>
 	import { scale } from 'svelte/transition';
 	import { flip } from 'svelte/animate';
-	
+
 	export let todos;
-	
+
 	async function patch(res) {
 		const todo = await res.json();
-	
+
 		todos = todos.map((t) => {
 			if (t.uid === todo.uid) return todo;
 			return t;
@@ -44,42 +44,65 @@
 <div class="todos">
 	<h1>Todos</h1>
 
-	<form class="new" action="/todos.json" method="post" use:enhance={{
-		result: async (res, form) => {
-			const created = await res.json();
-			todos = [...todos, created]
+	<form
+		class="new"
+		action="/todos.json"
+		method="post"
+		use:enhance={{
+			result: async (res, form) => {
+				const created = await res.json();
+				todos = [...todos, created];
 
-			form.reset();
-		}
-	}}>
-		<input name="text" aria-label="Add todo" placeholder="+ tap to add a todo">
+				form.reset();
+			}
+		}}
+	>
+		<input name="text" aria-label="Add todo" placeholder="+ tap to add a todo" />
 	</form>
 
 	{#each todos as todo (todo.uid)}
-		<div class="todo" class:done={todo.done} transition:scale|local={{start:0.7}} animate:flip={{duration:200}}>
-			<form action="/todos/{todo.uid}.json?_method=patch" method="post" use:enhance={{
-				pending: (data) => {
-					todo.done = !!data.get('done');
-				},
-				result: patch
-			}}>
-				<input type="hidden" name="done" value={todo.done ? '' : 'true'}>
-				<button class="toggle" aria-label="Mark todo as {todo.done ? 'not done' : 'done'}"/>
+		<div
+			class="todo"
+			class:done={todo.done}
+			transition:scale|local={{ start: 0.7 }}
+			animate:flip={{ duration: 200 }}
+		>
+			<form
+				action="/todos/{todo.uid}.json?_method=patch"
+				method="post"
+				use:enhance={{
+					pending: (data) => {
+						todo.done = !!data.get('done');
+					},
+					result: patch
+				}}
+			>
+				<input type="hidden" name="done" value={todo.done ? '' : 'true'} />
+				<button class="toggle" aria-label="Mark todo as {todo.done ? 'not done' : 'done'}" />
 			</form>
 
-			<form class="text" action="/todos/{todo.uid}.json?_method=patch" method="post" use:enhance={{
-				result: patch
-			}}>
-				<input aria-label="Edit todo" type="text" name="text" value={todo.text}>
-				<button class="save" aria-label="Save todo"/>
+			<form
+				class="text"
+				action="/todos/{todo.uid}.json?_method=patch"
+				method="post"
+				use:enhance={{
+					result: patch
+				}}
+			>
+				<input aria-label="Edit todo" type="text" name="text" value={todo.text} />
+				<button class="save" aria-label="Save todo" />
 			</form>
 
-			<form action="/todos/{todo.uid}.json?_method=delete" method="post" use:enhance={{
-				result: () => {
-					todos = todos.filter(t => t.uid !== todo.uid);
-				}
-			}}>
-				<button class="delete" aria-label="Delete todo"/>
+			<form
+				action="/todos/{todo.uid}.json?_method=delete"
+				method="post"
+				use:enhance={{
+					result: () => {
+						todos = todos.filter((t) => t.uid !== todo.uid);
+					}
+				}}
+			>
+				<button class="delete" aria-label="Delete todo" />
 			</form>
 		</div>
 	{/each}
@@ -102,7 +125,7 @@
 	}
 
 	input:focus-visible {
-		box-shadow: inset 1px 1px 6px rgba(0,0,0,0.1);
+		box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.1);
 		border: 1px solid #ff3e00 !important;
 		outline: none;
 	}
@@ -112,7 +135,7 @@
 		width: 100%;
 		padding: 0.5em 1em 0.3em 1em;
 		box-sizing: border-box;
-		background: rgba(255,255,255,0.05);
+		background: rgba(255, 255, 255, 0.05);
 		border-radius: 8px;
 		text-align: center;
 	}
@@ -126,16 +149,15 @@
 		padding: 0.5rem;
 		background-color: white;
 		border-radius: 8px;
-		filter: drop-shadow(2px 4px 6px rgba(0,0,0,0.1));
+		filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 0.1));
 		transform: translate(-1px, -1px);
 		transition: filter 0.2s, transform 0.2s;
 	}
 
-
 	.done {
 		transform: none;
 		opacity: 0.4;
-		filter: drop-shadow(0px 0px 1px rgba(0,0,0,0.1));
+		filter: drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.1));
 	}
 
 	form.text {
@@ -161,7 +183,7 @@
 	}
 
 	button.toggle {
-		border: 1px solid rgba(0,0,0,0.2);
+		border: 1px solid rgba(0, 0, 0, 0.2);
 		border-radius: 50%;
 		box-sizing: border-box;
 		background-size: 1em auto;
